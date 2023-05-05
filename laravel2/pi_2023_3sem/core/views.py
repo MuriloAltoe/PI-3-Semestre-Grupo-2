efe716030e27675818f798c5686d1979b42f9ee3
MuriloAltoe <muriloaleme17@gmail.com>
2023-05-04 22:08:40 -0300

    djongo-add

--- laravel2/pi_2023_3sem/core/views.py
+++ laravel2/pi_2023_3sem/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from core.utils import get_db_handle, get_collection_handle
+db_handle, mongo_client = get_db_handle(DATABASE_NAME, DATABASE_HOST, DATABASE_PORT, USERNAME, PASSWORD)
+collection_handle = get_collection_handle(db_handle, REGIONS_COLLECTION)
+collection_handle.find({...})
+collection_handle.insert({...})
+collection_handle.update({...})
