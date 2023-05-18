44b463828e1beae81586ec23f5dd50d777ccf404
Vitor Villa <80797997+VitorVilla@users.noreply.github.com>
2023-05-18 20:43:35 -0300

    modeladd

--- pi_2023_3semestre/core/models.py
+++ pi_2023_3semestre/core/models.py
@@ -1,25 +1,30 @@
 from django.db import models
 from djongo import models as djongo_models
 
-class Usuario(djongo_models.Model):
-    # id_usuario = models.IntegerField(verbose_name='usuario_', unique=True)
-    email = models.EmailField(verbose_name='email_', unique=True)
-
-    class Meta:
-        db_table = 'usuarios'
-
-
-class Barraca(djongo_models.Model):
-    id_usuario = models.IntegerField(verbose_name='usuario_', unique=True)
-    id_itens = models.IntegerField(verbose_name='items_', unique=True)
-
-
-    class Meta:
-        db_table = 'barraca'
-
 
 class Itens(djongo_models.Model):
-    id_barraca = models.EmailField(verbose_name='email_', unique=True)
-
+    nome = models.CharField(verbose_name="nome", max_length=100)
+    preco = models.DecimalField(verbose_name="preco", max_digits=8, decimal_places=2)
+    categoria = models.CharField(verbose_name="categoria", max_length=100)
+    quantidade = models.IntegerField(verbose_name="quantidade")
     class Meta:
         db_table = 'itens'
+        
+        
+class Usuario(djongo_models.Model):
+    email = models.EmailField(verbose_name="email",unique=True)
+    produtor = models.BooleanField(verbose_name="podutor", )
+    senha = models.CharField(verbose_name="senha", max_length=100)
+    telefone = models.CharField(verbose_name="telefone", max_length=100)
+    entrega = models.BooleanField(verbose_name="entrega", default=False)
+    #endereco
+    rua = models.CharField(verbose_name="rua", max_length=100)
+    cidade = models.CharField(verbose_name="cidade", max_length=100)
+    cep = models.CharField(verbose_name="cep", max_length=100)
+    estado = models.CharField(verbose_name="estado", max_length=100)
+    complemento = models.CharField(verbose_name="complemento", max_length=100)
+    bairro = models.CharField(verbose_name="bairro", max_length=100)
+    numero = models.IntegerField(verbose_name="numero")
+    
+    class Meta:
+        db_table = 'usuario'
--- pi_2023_3semestre/pi_2023_3semestre/settings.py
+++ pi_2023_3semestre/pi_2023_3semestre/settings.py
@@ -78,8 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'banco',
-        'HOST': 'localhost',
-        'PORT': '27017'
+        'CLIENT': {
+            'host': 'localhost',
+            'port': 27017,
+        },
     }
 }
 
