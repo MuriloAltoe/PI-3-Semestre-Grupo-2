77f899bb0a04260084711b900a97e61c44b23e47
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2023-06-03 10:55:44 -0300

    Adicionando modais de produtos

--- projeto-front-end/projeto-angular/projeto-inicial/src/app/pages/usuario/usuario/usuario/usuario.component.ts
+++ projeto-front-end/projeto-angular/projeto-inicial/src/app/pages/usuario/usuario/usuario/usuario.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./usuario.component.scss']
 })
 export class UsuarioComponent {
+  currentStep = 1;
+  totalSteps = 3;
 
+  setCurrentStep(step: number) {
+    this.currentStep = step;
+  }
+
+  nextStep() {
+    if (this.currentStep < this.totalSteps) {
+      this.currentStep++;
+    }
+  }
+
+  previousStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
+
+  isLastStep() {
+    return this.currentStep === this.totalSteps;
+  }
 }
